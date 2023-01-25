4bf33d4eadc770a3fe8380e378baa6be44e64a22
mbehboodian <29199390+xeptore@users.noreply.github.com>
2023-01-25 07:33:20 +0330

    feat(web): add tailwindcss integration
    Signed-off-by: mbehboodian <29199390+xeptore@users.noreply.github.com>

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -7,4 +8,5 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone'
   }),
+  integrations: [tailwind()]
 });
