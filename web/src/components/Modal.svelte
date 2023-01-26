436e8312a6bc90e4bf6332bed72d5b7bc3a7e2fe
mbehboodian <29199390+xeptore@users.noreply.github.com>
2023-01-26 23:03:21 +0330

    feat(web): add transitioning create new client modal
    Signed-off-by: mbehboodian <29199390+xeptore@users.noreply.github.com>

--- web/src/components/Modal.svelte
+++ web/src/components/Modal.svelte
@@ -1,52 +1,53 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  import { createEventDispatcher } from "svelte";
+  import { fly } from "svelte/transition";
+  const dispatch = createEventDispatcher();
 
-  let backdrop: HTMLElement | null;
-  let modal: HTMLElement | null;
-
-  function handleClick() {
-    modal?.classList.add("hidden");
-    backdrop?.classList.add("hidden");
+  function closeModal() {
+    dispatch("close");
   }
 
-  onMount(() => {
-    backdrop?.addEventListener("click", handleClick);
-
-    return () => {
-      backdrop?.removeEventListener("click", handleClick);
-    };
-  });
+  function handleKeyPress(e: KeyboardEvent) {
+    if (e.key === "Escape") {
+      dispatch("close");
+    }
+  }
 </script>
 
+<svelte:window on:keydown={handleKeyPress} />
 <div
-  bind:this={backdrop}
+  on:click|once={closeModal}
+  aria-hidden="true"
   class="fixed top-0 left-0 w-full h-full bg-[#00000096]"
 />
 
 <div
   class="modal fixed top-0 left-0 fade outline-none"
-  bind:this={modal}
   aria-modal="true"
   role="dialog"
 >
   <div
+    in:fly={{ y: 200, duration: 300 }}
+    out:fly={{ y: 200, duration: 300 }}
     class="modal-dialog modal-dialog-scrollable pointer-events-none fixed flex h-full"
   >
     <div
-      class="modal-content border-none shadow-lg flex flex-col w-3/5 h-5/6 m-auto pointer-events-auto text-white bg-gray-700 rounded-md outline-none text-current"
+      class="modal-content border-none shadow-lg flex flex-col w-2/3 h-5/6 m-auto pointer-events-auto text-white bg-gray-800 rounded-md outline-none text-current"
     >
       <div
-        class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-500 rounded-t-md"
+        class="modal-header flex items-center justify-between p-4 border-b border-gray-500 rounded-t-md"
       >
         <h5 class="text-xl font-medium leading-normal text-white">
           Modal title
         </h5>
         <button
           type="button"
-          class="btn-close box-content w-4 h-4 p-1 border-none rounded-none opacity-75 focus:shadow-none focus:outline-none focus:opacity-100 hover:opacity-75 hover:no-underline"
-          data-bs-dismiss="modal"
-          aria-label="Close">x</button
+          class="btn-close box-content w-4 h-4 p-1 border-none rounded-none opacity-75 focus:shadow-none focus:outline-none hover:opacity-100 hover:no-underline"
+          on:click={closeModal}
+          aria-label="Close"
         >
+          <i class="fa-solid fa-xmark align-text-top" />
+        </button>
       </div>
       <div
         class="modal-body dark-scrollbars relative p-4 max-h-[76vh] overflow-hidden overflow-y-scroll"
@@ -69,16 +70,16 @@
       >
         <button
           type="button"
-          class="inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
-          data-bs-dismiss="modal"
+          class="inline-block px-6 py-2.5 bg-wireguard-800 border border-wireguard-50 text-wireguard-50 text-xs uppercase rounded shadow-md hover:bg-wireguard-600 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
+          on:click={closeModal}
         >
-          Close
+          Cancel
         </button>
         <button
           type="button"
-          class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ml-1"
+          class="inline-block px-6 py-2.5 bg-wireguard-200 border border-wireguard-200 text-white text-xs uppercase rounded shadow-md hover:bg-wireguard-600 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ml-1"
         >
-          Save changes
+          Create
         </button>
       </div>
     </div>
