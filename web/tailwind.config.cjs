3605148f62ecd607882e6b38a5a3eada10b071cd
mbehboodian <29199390+xeptore@users.noreply.github.com>
2023-01-26 23:01:52 +0330

    feat(web): add wireguard color palette
    Signed-off-by: mbehboodian <29199390+xeptore@users.noreply.github.com>

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -7,6 +7,20 @@ module.exports = {
       fontFamily: {
         cantarell: ["Cantarell", "sans-serif"],
       },
+      colors: {
+        wireguard: {
+          50: "#C02126",
+          100: "#AE1E22",
+          200: "#9D1B1F",
+          300: "#88171A",
+          400: "#7A1518",
+          500: "#691215",
+          600: "#570F11",
+          700: "#460C0E",
+          800: "#34090A",
+          900: "#230607",
+        },
+      },
     },
   },
   plugins: [],
