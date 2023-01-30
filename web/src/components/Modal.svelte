7fdc20c8b31385794feae7c522f5b650d03b9a84
mbehboodian <29199390+xeptore@users.noreply.github.com>
2023-01-30 19:48:00 +0330

    fix(web): add form validations
    Signed-off-by: mbehboodian <29199390+xeptore@users.noreply.github.com>

--- web/src/components/Modal.svelte
+++ web/src/components/Modal.svelte
@@ -3,6 +3,51 @@
   import { createEventDispatcher } from "svelte";
   import { fly } from "svelte/transition";
 
+  let formTouched = false;
+  let nameShowValidationError = false;
+  let nameValidationError: string | null = "Required";
+  let descriptionShowValidationError = false;
+  let descriptionValidationError: string | null = null;
+
+  $: nameIsValid = nameValidationError === null;
+  $: descriptionIsValid = descriptionValidationError === null;
+  $: submitEnabled = formTouched && nameIsValid && descriptionIsValid;
+
+  function handleNameInput(v: string) {
+    if (v.trim().length === 0) {
+      nameValidationError = "Required";
+      return;
+    }
+
+    if (v.trim().length !== v.length) {
+      nameValidationError = "Invalid value";
+      return;
+    }
+
+    if (v.length > 256) {
+      nameValidationError = "Cannot be longer than 256 characters";
+      return;
+    }
+
+    nameValidationError = null;
+    return;
+  }
+
+  function handleDescriptionInput(v: string) {
+    if (v.trim().length !== v.length) {
+      nameValidationError = "Invalid value";
+      return;
+    }
+
+    if (v.length > 10_000) {
+      descriptionValidationError = "Cannot be longer than 10,000 characters";
+      return;
+    }
+
+    descriptionValidationError = null;
+    return;
+  }
+
   const dispatch = createEventDispatcher();
 
   function closeModal() {
@@ -65,7 +110,7 @@
     class="modal-dialog modal-dialog-scrollable pointer-events-none fixed flex h-full w-full"
   >
     <div
-      class="modal-content border-none shadow-lg flex flex-col w-2/3 m-auto pointer-events-auto text-white bg-gray-800 rounded-md outline-none text-current"
+      class="modal-content border-none shadow-lg flex flex-col w-2/3 lg:w-1/2 xl:w-1/2 2xl:w-3/7 m-auto pointer-events-auto text-white bg-gray-800 rounded-md outline-none text-current"
     >
       <div
         class="modal-header flex items-center justify-between p-4 border-b border-gray-500 rounded-t-md"
@@ -94,30 +139,52 @@
               autocomplete="name"
               class="px-3 py-2 max-w-full border-gray-700 rounded w-full dark:placeholder-gray-400 focus:ring focus:ring-blue-600 focus:border-blue-600 focus:outline-none h-12 border bg-gray-800"
               placeholder="Ali Agha"
+              on:focusin|once|self|trusted|capture={() => {
+                formTouched = true;
+              }}
+              on:focusout|once|self|trusted|capture={() => {
+                nameShowValidationError = true;
+              }}
+              on:input|self|trusted|capture={(e) => {
+                handleNameInput(e.currentTarget.value);
+              }}
             />
+            {#if nameShowValidationError && nameValidationError !== null}
+              <p class="mt-2 text-pink-600">{nameValidationError}</p>
+            {/if}
           </div>
           <div class="mb-6 last:mb-0">
             <label class="block font-bold mb-2" for="description"
               >Description</label
             >
-            <div>
-              <div class="relative">
-                <input
-                  id="description"
-                  name="description"
-                  type="description"
-                  autocomplete="off"
-                  class="px-3 py-2 max-w-full border-gray-700 rounded w-full dark:placeholder-gray-400 focus:ring focus:ring-blue-600 focus:border-blue-600 focus:outline-none h-12 border bg-gray-800"
-                  placeholder="Here goes some description..."
-                />
-              </div>
-            </div>
+            <input
+              id="description"
+              name="description"
+              type="description"
+              autocomplete="off"
+              class="px-3 py-2 max-w-full border-gray-700 rounded w-full dark:placeholder-gray-400 focus:ring focus:ring-blue-600 focus:border-blue-600 focus:outline-none h-12 border bg-gray-800"
+              placeholder="Here goes some description..."
+              on:focusin|once|self|trusted|capture={() => {
+                formTouched = true;
+              }}
+              on:focusout|once|self|trusted|capture={() => {
+                descriptionShowValidationError = true;
+              }}
+              on:input|self|trusted|capture={(e) => {
+                handleDescriptionInput(e.currentTarget.value);
+              }}
+            />
+            {#if descriptionShowValidationError && descriptionValidationError !== null}
+              <p class="mt-2 text-pink-600">{descriptionValidationError}</p>
+            {/if}
           </div>
           <hr class="my-6 -mx-6 dark:border-gray-800 border-t" />
           <div class="-mb-3 text-center">
             <button
               class="w-full focus:outline-none uppercase transition-colors focus:ring duration-150 border cursor-pointer rounded-lg border-violet-600 ring-violet-700 bg-violet-500 text-white hover:bg-violet-600 mb-3 py-2 px-3"
               type="submit"
+              class:disabled={!submitEnabled}
+              disabled={!submitEnabled}
             >
               <span class="px-2">Create</span>
             </button>
@@ -127,3 +194,11 @@
     </div>
   </div>
 </div>
+
+<style>
+  button.disabled {
+    cursor: not-allowed;
+    background-color: dimgray;
+    border-color: rgb(143, 143, 143);
+  }
+</style>
---
+++ web/src/forms/Login.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+  let formTouched = false;
+  let usernameShowValidationError = false;
+  let usernameValidationError: string | null = "Required";
+  let passwordShowValidationError = false;
+  let passwordValidationError: string | null = "Required";
+
+  $: usernameIsValid = usernameValidationError === null;
+  $: passwordIsValid = passwordValidationError === null;
+  $: submitEnabled = formTouched && usernameIsValid && passwordIsValid;
+
+  function handleUsernameInput(v: string) {
+    if (v.trim().length === 0) {
+      usernameValidationError = "Required";
+      return;
+    }
+
+    if (v.trim().length !== v.length) {
+      usernameValidationError = "Invalid value";
+      return;
+    }
+
+    if (v.length > 64) {
+      usernameValidationError = "Cannot be longer than 64 characters";
+      return;
+    }
+
+    usernameValidationError = null;
+    return;
+  }
+
+  function handlePasswordInput(v: string) {
+    if (v.trim().length === 0) {
+      passwordValidationError = "Required";
+      return;
+    }
+
+    if (v.trim().length !== v.length) {
+      passwordValidationError = "Invalid value";
+      return;
+    }
+
+    if (v.length > 128) {
+      passwordValidationError = "Cannot be longer than 128 characters";
+      return;
+    }
+
+    passwordValidationError = null;
+    return;
+  }
+</script>
+
+<form action="/auth/login" method="post">
+  <div class="mb-6 last:mb-0">
+    <label class="block font-bold mb-2" for="username">Username</label>
+    <input
+      id="username"
+      name="username"
+      autocomplete="email"
+      class="px-3 py-2 max-w-full border-gray-700 rounded w-full dark:placeholder-gray-400 focus:ring focus:ring-blue-600 focus:border-blue-600 focus:outline-none h-12 border bg-gray-800"
+      placeholder="john.doe"
+      on:focusin|once|self|trusted|capture={() => {
+        formTouched = true;
+      }}
+      on:focusout|once|self|trusted|capture={() => {
+        usernameShowValidationError = true;
+      }}
+      on:input|self|trusted|capture={(e) => {
+        handleUsernameInput(e.currentTarget.value);
+      }}
+    />
+    {#if usernameShowValidationError && usernameValidationError !== null}
+      <p class="mt-2 text-pink-600">{usernameValidationError}</p>
+    {/if}
+  </div>
+  <div class="mb-6 last:mb-0">
+    <label class="block font-bold mb-2" for="password">Password</label>
+    <input
+      id="password"
+      name="password"
+      type="password"
+      autocomplete="current-password"
+      class="px-3 py-2 max-w-full border-gray-700 rounded w-full dark:placeholder-gray-400 focus:ring focus:ring-blue-600 focus:border-blue-600 focus:outline-none h-12 border bg-gray-800"
+      placeholder="**********"
+      on:focusin|once|self|trusted|capture={() => {
+        formTouched = true;
+      }}
+      on:focusout|once|self|trusted|capture={() => {
+        passwordShowValidationError = true;
+      }}
+      on:input|self|trusted|capture={(e) => {
+        handlePasswordInput(e.currentTarget.value);
+      }}
+    />
+    {#if passwordShowValidationError && passwordValidationError !== null}
+      <p class="mt-2 text-pink-600">{passwordValidationError}</p>
+    {/if}
+  </div>
+  <hr class="my-6 -mx-6 dark:border-gray-800 border-t" />
+  <div class="-mb-3 text-center">
+    <button
+      class="w-full focus:outline-none uppercase transition-colors focus:ring duration-300 border cursor-pointer rounded-lg border-violet-600 ring-violet-700 bg-violet-500 text-white hover:bg-violet-600 mb-3 py-2 px-3"
+      type="submit"
+      class:disabled={!submitEnabled}
+      disabled={!submitEnabled}
+    >
+      <span class="px-2">Sign in</span>
+    </button>
+  </div>
+</form>
+
+<style>
+  button.disabled {
+    cursor: not-allowed;
+    background-color: dimgray;
+    border-color: rgb(143, 143, 143);
+  }
+</style>
